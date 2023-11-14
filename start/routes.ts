0f8b5d847b630f18d4c978fdc38c79ee4f3adabe
Refri Rifwan <refririfwan@gmail.com>
2023-11-14 18:44:34 +0700

    Create Endpoint Api For Login #11

--- start/routes.ts
+++ start/routes.ts
@@ -22,4 +22,5 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.post('/auth/register', 'AuthController.register')
+  Route.post('/auth/login', 'AuthController.login')
 }).prefix('/api')
